02a14cf43f28d18ee4265082d6cb443028fb1410
Camo <64373650+CamooCodee@users.noreply.github.com>
2020-10-10 17:03:46 +0200

    basic user input done

---
+++ 2d Project_v0.1/Assets/Scripts/User/Input/UserInput.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+
+namespace UserInput
+{
+    // Written by Lukas Sacher/Camo
+    public static class UserInput
+    {
+        static bool isSetup = false;
+
+        static UserInputSettings inputSettings;
+
+        // movement
+        public static Action moveForward;
+        public static Action moveBackwards;
+        public static Action moveLeft;
+        public static Action moveRight;
+
+        // pickup
+        public static Action pickUp;
+        public static Action drop;
+
+        // menus
+        public static Action toggleInventory;
+
+        public static void Setup()
+		{
+            inputSettings = UserInputSettings.current;
+            isSetup = true;
+		}
+
+        public static void Update()
+		{
+			if (!isSetup)
+			{
+                throw new Exception("The UserInput is not setup! Make sure there is a game object with the UserInputSettings script");
+			}
+
+            MovementInput();
+            PickUpInput();
+            MenuInput();
+		}
+
+		#region InputFuncs
+		static void MovementInput()
+		{
+			if (Input.GetKey(inputSettings.forward))
+			{
+                moveForward?.Invoke();
+			}
+            if (Input.GetKey(inputSettings.backwards))
+            {
+                moveBackwards?.Invoke();
+            }
+            if (Input.GetKey(inputSettings.left))
+            {
+                moveLeft?.Invoke();
+            }
+            if (Input.GetKey(inputSettings.right))
+            {
+                moveRight?.Invoke();
+            }
+        }
+        static void PickUpInput()
+		{
+            if (Input.GetKeyDown(inputSettings.pickUp))
+            {
+                pickUp?.Invoke();
+            }
+            if (Input.GetKeyDown(inputSettings.drop))
+            {
+                drop?.Invoke();
+            }
+        }
+        static void MenuInput()
+		{
+            if (Input.GetKeyDown(inputSettings.toggleInventory))
+            {
+                toggleInventory?.Invoke();
+            }
+        }
+		#endregion
+	}
+}
---
+++ 2d Project_v0.1/Assets/Scripts/User/Input/UserInputSettings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserInput
+{
+	// Written by Lukas Sacher/Camo
+    public class UserInputSettings : MonoBehaviour
+    {
+        public static UserInputSettings current = null;
+
+		// movement
+		public KeyCode forward;
+		public KeyCode backwards;
+		public KeyCode left;
+		public KeyCode right;
+		[Space(10f)] // pickup
+		public KeyCode pickUp;
+		public KeyCode drop;
+		[Space(10f)] // menus
+		public KeyCode toggleInventory;
+
+		private void Awake()
+		{
+			if(current.gameObject == null)
+			{
+				current = this;
+			}
+			else
+			{
+				throw new System.Exception("Ther error!");
+			}
+		}
+
+		private void Start()
+		{
+			UserInput.Setup();
+		}
+
+		private void Update()
+		{
+			UserInput.Update();
+		}
+	}
+}
