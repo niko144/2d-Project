1f79aa405466a443bbb2558739fec7b9007a761b
Camo <64373650+CamooCodee@users.noreply.github.com>
2020-10-11 18:52:04 +0200

    Chunk System

---
+++ 2d Project_v0.1/Assets/Scripts/Technical/Chunk.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chunks
+{
+
+    // Written by Lukas Sacher/Camo
+    public class Chunk : MonoBehaviour
+    {
+		public const float CHUNK_SIZE = 12f;
+
+		GameObject container = null;
+
+		GameObject player = null;
+		float loadingDist = 0f;
+
+		private void Start()
+		{
+			player = ChunkMaster.current.player;
+			loadingDist = ChunkMaster.current.loadingDistance;
+
+			container = transform.GetChild(0).gameObject;
+			if (!container.transform.name.Contains("Container"))
+			{
+				Debug.LogWarning("The content of a chunks has to be in a seperate container.");
+			}
+		}
+
+		private void Update()
+		{
+			CheckForChunkLoad();
+		}
+
+		void CheckForChunkLoad()
+		{
+			container.SetActive(Vector2.Distance(transform.position, player.transform.position) <= loadingDist);
+		}
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.white;
+
+			Vector2[] points = new Vector2[4];
+
+			float halfSize = CHUNK_SIZE * 0.5f;
+
+			points[0] = new Vector2(transform.position.x - halfSize, transform.position.y + halfSize); 
+			points[1] = new Vector2(transform.position.x + halfSize, transform.position.y + halfSize); 
+			points[2] = new Vector2(transform.position.x + halfSize, transform.position.y - halfSize); 
+			points[3] = new Vector2(transform.position.x - halfSize, transform.position.y - halfSize);
+
+			for (int i = 0; i < points.Length; i++)
+			{
+				if(i != points.Length - 1)
+				{
+					Gizmos.DrawLine(points[i], points[i + 1]);
+				}
+				else
+				{
+					Gizmos.DrawLine(points[i], points[0]);
+				}
+			}
+		}
+	}
+}
---
+++ 2d Project_v0.1/Assets/Scripts/Technical/ChunkMaster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Chunks
+{
+    // Written by Lukas Sacher/Camo
+    public class ChunkMaster : MonoBehaviour
+    {
+        public static ChunkMaster current = null;
+
+        public GameObject player;
+        [Space(10f)]
+        public float loadingDistance;
+
+        private void Awake()
+        {
+            if (current == null || current.gameObject == null)
+            {
+                current = this;
+            }
+            else
+            {
+                throw new System.Exception("Make sure there is only one game object with the ChunkMaster script attached!");
+            }
+        }
+    }
+}
