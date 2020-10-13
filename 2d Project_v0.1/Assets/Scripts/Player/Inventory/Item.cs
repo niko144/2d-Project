4ca802832efdb624e083e59017d46a6b47013a65
Camo <64373650+CamooCodee@users.noreply.github.com>
2020-10-13 16:55:00 +0200

    Start of inventory

---
+++ 2d Project_v0.1/Assets/Scripts/Player/Inventory/Item.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace PlayerInventory
+{
+    // Written by Lukas Sacher / Camo
+    public class Item : ScriptableObject
+    {
+        public string itemName;
+        public int stackSize = 16;
+        public Sprite sprite;
+
+        string itemId;
+        bool hasId = false;
+
+        public void GenerateItemId()
+		{
+			if (itemName.Contains("\t") || itemName.Contains("\n") || itemName.Contains(" "))
+			{
+                throw new System.Exception("Item names should always be a string without spaces!");
+			}
+
+            itemId = $"item_data: {itemName}_{stackSize}_";
+            hasId = true;
+		}
+        public string GetItemId()
+		{
+			if (!hasId)
+			{
+                GenerateItemId();
+			}
+            return itemId;
+		}
+    }
+}
---
+++ 2d Project_v0.1/Assets/Scripts/Player/Inventory/PlayerInventory.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+namespace PlayerInventory
+{
+	// Written by Lukas Sacher / Camo
+	// Idea by Lukas Sacher and Muneeb Ur Rahman
+	public class PlayerInventory : MonoBehaviour
+    {
+        public int hotbarSlotAmount;
+        public int inventorySlotAmount;
+
+        [HideInInspector] public ItemStack[] hotbar;
+        [HideInInspector] public ItemStack[] inventory;
+
+		bool hotbarIsFull = false;
+		bool inventoryIsFull = false;
+
+		private void Start()
+		{
+			SetupInventory();
+		}
+
+		void SetupInventory()
+		{
+			hotbar = new ItemStack[hotbarSlotAmount];
+			inventory = new ItemStack[inventorySlotAmount];
+		}
+
+		/// <summary>
+		/// Adds a stack of items to the inventory.
+		/// </summary>
+		/// <returns>Returns if the stack could be added.</returns>
+		public bool AddStack(ItemStack stack)
+		{
+			if (!hotbarIsFull)
+			{
+				AddStackToHotbar(stack);
+				return true;
+			}
+			else if (!inventoryIsFull)
+			{
+
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		#region AddStackFunctions_ForCleanCode
+		void AddStackToHotbar(ItemStack stack)
+		{
+			for (int i = 0; i < hotbar.Length; i++)
+			{
+				if (hotbar[i].itemId == stack.itemId)
+				{
+
+				}
+			}
+		}
+		#endregion
+		/// <summary>
+		/// Adds an item to the inventory.
+		/// </summary>
+		/// <returns>Returns if the item could be added.</returns>
+		public bool AddItem(Item item)
+		{
+			if(hotbarIsFull && inventoryIsFull)
+			{
+				return false;
+			}
+			else
+			{
+				AddStack(new ItemStack(item.GetItemId(), 1));
+				return true;
+			}
+		}
+    }
+
+    public class ItemStack
+	{
+		public string itemId;
+		public int size;
+
+		public ItemStack(string itemId, int size)
+		{
+			this.size = size;
+			this.itemId = itemId;
+		}
+	}
+}
