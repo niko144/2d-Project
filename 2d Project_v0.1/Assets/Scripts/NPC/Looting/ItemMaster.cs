70f0ebbc67638889a650a13f469179a67ff5cfa9
Camo <64373650+CamooCodee@users.noreply.github.com>
2020-10-18 10:19:03 +0200

    Sandbox Scenes and Prems Looting system

---
+++ 2d Project_v0.1/Assets/Scripts/NPC/Looting/ItemMaster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace NPC.Drop.Items
+{
+    // Written by Prem
+    public class ItemMaster : MonoBehaviour
+    {
+        // if access to player 
+        //private Player player;
+
+        public delegate void GeneralEventHandler();
+        public event GeneralEventHandler EventObjectThrow;
+
+        public void CallEventObjectThrow()
+        {
+            EventObjectThrow?.Invoke();
+        }
+    }
+}
---
+++ 2d Project_v0.1/Assets/Scripts/NPC/Looting/NPCDropItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using NPC.Drop.Items;
+
+namespace NPC.Drop
+{
+    // Written by Prem
+    public class NPCDropItem : MonoBehaviour
+    {
+        private NPCMaster npcMaster;
+        public GameObject[] itemToDrop;
+
+
+        void OnEnable()
+        {
+            SetInitialReferences();
+            npcMaster.EventNPCDie += DropItems;
+        }
+        void OnDisable()
+        {
+            npcMaster.EventNPCDie -= DropItems;
+        }
+
+
+        void SetInitialReferences()
+        {
+            npcMaster = GetComponent<NPCMaster>();
+        }
+
+        void DropItems()
+        {
+            if (itemToDrop.Length > 0)
+            {
+                foreach (GameObject item in itemToDrop)
+                {
+                    StartCoroutine(PauseBeforeDrop(item)); //otherwise the event gets fired before the start
+                }
+            }
+        }
+
+
+        IEnumerator PauseBeforeDrop(GameObject itemToDrop)
+        {
+            yield return new WaitForSeconds(0.05f);
+            itemToDrop.SetActive(true);
+            itemToDrop.transform.SetParent(null);
+            yield return new WaitForSeconds(0.05f);
+            if (itemToDrop.GetComponent<ItemMaster>() != null)
+            {
+                itemToDrop.GetComponent<ItemMaster>().CallEventObjectThrow();
+            }
+        }
+    }
+}
