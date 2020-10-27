23475164ceb049fbd736effdbf9c37b22bafb2f7
patrick123900 <p.patrickkirk@gmail.com>
2020-10-27 03:23:07 +0100

    Localization system added
    Localization system added. Can be tested in Pille-Sandbox.unity. Enter a string in English in the LocalizationTest component on the manager object, set the language to a language code and run. Then press space in the game and read the return value from the console. Make sure that the search word and language code exists in the localizationData.csv found in "Ressources\LocalizationData". NOTE: The system is case-sensitive

---
+++ 2d Project_v0.1/Assets/Scripts/Technical/Localization/LocalizationInit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationInit : MonoBehaviour
+{
+    public string language = "da";
+
+    void Start()
+    {
+        Localization.InitializeData(language);
+    }
+}
---
+++ 2d Project_v0.1/Assets/Scripts/Technical/Localization/LocalizationTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationTest : MonoBehaviour
+{
+    public string wordToGet = "Options";
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Localization.GetStringForLanguage(wordToGet));
+        }
+    }
+}
